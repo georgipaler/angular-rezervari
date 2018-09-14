feb58f70a6b41d69d4ba8e50718874a2a609353d
AzureAD\GeorgianaPaler <georgiana.paler@ibm.com>
2018-09-14 10:14:59 +0300

    all rooms component

---
+++ angular-rezervari/src/app/all-rooms/all-rooms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-rooms',
+  templateUrl: './all-rooms.component.html',
+  styleUrls: ['./all-rooms.component.css']
+})
+export class AllRoomsComponent implements OnInit {
+
+  constructor() { }
+
+  public  room: '1.';
+  ngOnInit() {
+  }
+
+}
